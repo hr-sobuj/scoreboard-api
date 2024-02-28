4144706816b856a228087561031550aa04e07338
hr-sobuj <sobujhd@gmail.com>
2024-02-28 23:42:09 +0600

    setup route

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
+
+// internal import 
+const authRoute=require('./route/authRoute')
+
+
 // create app object 
 const app = express();
 dotenv.config();
 app.use(cors());
+
 
 // db connection 
 mongoose.connect(
@@ -19,15 +25,16 @@ mongoose.connect(
     .catch((err) =>
         console.log(err.message));
 
+
 // parser 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// route 
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
+
+// routing setup
+app.use('/api/v1/auth',authRoute);
+
 
 // create server 
 app.listen(process.env.PORT || '5000', () => {
---
+++ route/authRoute.js
@@ -0,0 +1,7 @@
+// external import 
+const express=require('express');
+
+// create sub route 
+const route=express.Router();
+
+module.exports=route;
