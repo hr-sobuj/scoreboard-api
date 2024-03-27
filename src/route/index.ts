b465e066d4288e2cd984a0f907cbc9c9f728ede8
hr-sobuj <sobujhd@gmail.com>
2024-03-27 22:18:21 +0600

    fixed all issues

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import authRoute from './../app/modules/auth/auth.route';
+import scoreRoute from './../app/modules/score/score.route';
 
-const router = Router();
+const rootRouter = Router();
 
-export default router;
+rootRouter.use("/auth", authRoute);
+rootRouter.use("/score", scoreRoute)
+
+export default rootRouter;
